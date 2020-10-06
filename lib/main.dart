9425f8341ae830ca0feab8e1c3def48b551914cf
Tr3nt <David.Stevenson.Trent@gmail.com>
2020-10-06 10:38:07 -0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PhotoMemoApp());
+}
+
+class PhotoMemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello'),
+    );
+  }
+}
